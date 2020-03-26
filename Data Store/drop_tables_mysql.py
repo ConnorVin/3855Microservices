ef1bd5b318eed6de213cb259018cf85fb72dba79
Connor Vinchoff <cvinchoff@my.bcit.ca>
2020-03-26 11:45:48 -0700

    Config changes

--- Data Store/drop_tables_mysql.py
+++ Data Store/drop_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="localhost", user="root", password="password", database="events")
+db_conn = mysql.connector.connect(host="docker-3855.eastus.cloudapp.azure.com", user="root", password="password", database="events")
 
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
